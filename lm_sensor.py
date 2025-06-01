b7367cf53dff3acbb6826bd5574ad2a69da78e27
evatheunissen <eva.theunissen@student.vives.be>
2025-06-01 06:06:01 +0200

    edits 31-05-25

--- lm_sensor.py
+++ lm_sensor.py
@@ -5,9 +5,8 @@ class Sensor: # geen overerving van DistanceSensor want fout mbt verplichte defi
     distance_in_cm = 0
     distance_to_floor_in_cm = 300
     length_in_cm = 0
-    id = "none"
    
-    def __init__(self, echo_pin, trigger_pin, id = "none"):
+    def __init__(self, echo_pin, trigger_pin, id):
         if type(echo_pin) is int and type(trigger_pin) is int: 
             self.ds = DistanceSensor(echo = echo_pin, trigger = trigger_pin, max_distance = 3)
             try:
@@ -17,7 +16,7 @@ class Sensor: # geen overerving van DistanceSensor want fout mbt verplichte defi
                 raise  # fout opnieuw gegooid -> instantie niet aangemaakt
                 #del self
                 #print(f"Didn't find a distance sensor for given parameters (echo_pin & trigger_pin).")
-            self.id = id
+            self.id = str(id)
         else:
             raise TypeError("Parameters echo_pin en trigger_pin have to be of type 'int'.")
             #del self
@@ -31,12 +30,16 @@ class Sensor: # geen overerving van DistanceSensor want fout mbt verplichte defi
         else:
             self.distance_in_cm = 0
             print("No distance measured, setting distance_in_cm to 0.")
-        print(f"Measured distance: {self.distance_in_cm} cm.")
+        #print(f"Measured distance: {self.distance_in_cm} cm.")
 
     def calibrate(self):
         sleep(0.3)
-        if self.ds.distance is not None:
-            self.distance_to_floor_in_cm = self.ds.distance * 100
+        distance = self.ds.distance
+        if distance is not None:
+            self.distance_to_floor_in_cm = distance * 100
+        else:
+            self.distance_to_floor_in_cm = 300
+            print("No distance measured, setting distance_to_floor_in_cm to 300 cm.")
         
     def update(self):
         self.length_in_cm = self.distance_to_floor_in_cm - self.distance_in_cm
@@ -46,15 +49,16 @@ class Sensor: # geen overerving van DistanceSensor want fout mbt verplichte defi
         self.update()
 
 class SensorArray:
-    min_distance = 0
-    min_distance_sensor = 0
-    min_distance_sensor_index = 0
+    #min_distance = 0
+    #min_distance_sensor = 0
+    #min_distance_sensor_index = 0
     max_length = 0
+    max_length_sensor = None
     left_right_diff = 0
     front_back_diff = 0
     x_axis_calibrated = False
     y_axis_calibrated = False
-    average_min_distance = 0
+    #average_min_distance = 0
     average_max_length = 0
     average_left_right_diff = 0
     average_front_back_diff = 0
@@ -70,7 +74,7 @@ class SensorArray:
         elif type(sensor_array) is Sensor:
             self.sensors = [sensor_array]
         else:
-            raise TypeError("Parameter has to be of type 'Sensor'.")
+            raise TypeError("Parameter has to be of type 'list' with 'Sensor' type elements or 'Sensor'.")
             #del self
             #print(f"SensorArray object deleted, given parameter type was not Sensor.")
         
@@ -82,52 +86,91 @@ class SensorArray:
             sensor.measure_and_update()
             print(f"Distance in cm: {sensor.distance_in_cm}")
             print(f"Length in cm: {sensor.length_in_cm}")
-            if i == 0 or sensor.distance_in_cm < self.min_distance:
-                self.min_distance = sensor.distance_in_cm
-                self.min_distance_sensor = sensor
-                self.min_distance_sensor_index = i
+            if i == 0 or sensor.length_in_cm > self.max_length:
+                #self.min_distance = sensor.distance_in_cm
+                #self.min_distance_sensor = sensor
+                #self.min_distance_sensor_index = i
                 self.max_length = sensor.length_in_cm
-            print(f"New min distance: {self.min_distance}")
+                self.max_length_sensor = sensor
+            print(f"New max length: {self.max_length}")
+            if (self.max_length_sensor is not None):
+                print(f"New max length sensor: {self.max_length_sensor.id}")
             i += 1
 
     def calibrate_all(self):
+        with open("calibration.txt", "w") as f:
+            f.write("")
+
         for sensor in self.sensors:
             sensor.calibrate()
             print(f"{sensor.distance_to_floor_in_cm}")
-        # normaal verandert aantal sensors van object niet (probleem bij lager aantal), maar voor de zekerheid:
+            with open("calibration.txt", "a") as f:
+                f.write(f"{sensor.id}: {sensor.distance_to_floor_in_cm}\n")
+
+        # normaal verandert aantal sensors van object niet (probleem bij lager aantal -> nu bij zelfde aantal maar andere posities ook), maar voor de zekerheid:
         self.front_back_diff = 0
         self.left_right_diff = 0
         self.y_axis_calibrated = False
         self.x_axis_calibrated = False
-        if len(self.sensors) > 1:
-            self.front_back_diff = self.sensors[0].distance_to_floor_in_cm - self.sensors[len(self.sensors) - 1].distance_to_floor_in_cm
+
+        #if len(self.sensors) > 1:
+        #    self.front_back_diff = self.sensors[0].distance_to_floor_in_cm - self.sensors[len(self.sensors) - 1].distance_to_floor_in_cm
+        #    self.y_axis_calibrated = True
+        #    if len(self.sensors) > 3:
+        #        self.left_right_diff = self.sensors[1].distance_to_floor_in_cm - self.sensors[len(self.sensors) - 2].distance_to_floor_in_cm
+        #        self.x_axis_calibrated = True
+        front_sensor = next((s for s in self.sensors if s.id == "FRONT"), None)
+        back_sensor = next((s for s in self.sensors if s.id == "BACK"), None)
+        left_sensor = next((s for s in self.sensors if s.id == "LEFT"), None)
+        right_sensor = next((s for s in self.sensors if s.id == "RIGHT"), None)
+        if front_sensor and back_sensor:
+            self.front_back_diff = front_sensor.distance_to_floor_in_cm - back_sensor.distance_to_floor_in_cm
             self.y_axis_calibrated = True
-            if len(self.sensors) > 3:
-                self.left_right_diff = self.sensors[1].distance_to_floor_in_cm - self.sensors[len(self.sensors) - 2].distance_to_floor_in_cm
-                self.x_axis_calibrated = True
+        if left_sensor and right_sensor:
+            self.left_right_diff = left_sensor.distance_to_floor_in_cm - right_sensor.distance_to_floor_in_cm
+            self.x_axis_calibrated = True
             
     def update_all(self, shots):
         if type(shots) is int and shots != 0:
-            total_min_distance = 0
+            #total_min_distance = 0
             total_max_length = 0
             for s in range(shots):
                 self.measure_all()
-                total_min_distance += self.min_distance
+                #total_min_distance += self.min_distance
                 total_max_length += self.max_length
-            self.average_min_distance = total_min_distance / shots
+            #self.average_min_distance = total_min_distance / shots
             self.average_max_length = total_max_length / shots
         else:
             print(f"Parameter 'shots' has to be an int and not 0.")
             
     def stabilize(self, shots):
-        if type(shots) is int and shots != 0:
+        if type(shots) is int and shots > 0:
             total_front_back_diff = 0
             total_left_right_diff = 0
+            self.average_front_back_diff = 0
+            self.average_left_right_diff = 0
+
+            total_dtfic_array = []
+
             for s in range(shots):
                 self.calibrate_all()
+
                 total_front_back_diff += self.front_back_diff
                 total_left_right_diff += self.left_right_diff
-            self.average_front_back_diff = total_front_back_diff / shots
-            self.average_left_right_diff = total_left_right_diff / shots
+                
+                i = 0
+                for sensor in self.sensors:
+                    total_dtfic_array[i] += sensor.distance_to_floor_in_cm
+                    i += 1
+
+            if (self.y_axis_calibrated):
+                self.average_front_back_diff = total_front_back_diff / shots
+            if (self.x_axis_calibrated):
+                self.average_left_right_diff = total_left_right_diff / shots
+            
+            i = 0
+            for sensor in self.sensors:
+                sensor.distance_to_floor_in_cm = total_dtfic_array[i] / shots
+                i += 1
         else:
             print(f"Parameter 'shots' has to be an int and not 0.")
--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from machine import Pin
 from lm_settings import load_SensorArray
 from lm_sensor import Sensor
 
-s = Sensor(14, 13)
+# s = Sensor(14, 13, "FRONT")  # FRONT
 
 sa = load_SensorArray()
-for sensor in sa.sensors:
-    print(f"Sensor: {sensor.distance_in_cm} cm.")
+with open("calibration.txt", "r") as f:
+    content = f.read()
+if content and content != "":
+    for sensor in sa.sensors:
+        index = content.find(sensor.id)
+        if index != -1:
+            end_index = content.find("\n", index)
+            sensor.distance_to_floor_in_cm = content[index + len(sensor.id) + 2:end_index].strip()
 
 while True:
     cmd = input("> ")
@@ -15,13 +21,88 @@ while True:
         print("Exiting...")
         sys.exit(0)
     elif cmd == "measure":
-        print("Measuring distance...")
-        s.measure()
-        sa.sensors[1].measure()
-        dic = sa.sensors[1].distance_in_cm
-        print("Distance in cm: " + str(s.distance_in_cm) + ".\r\n")
+        print("Measuring sensor...")
+        sa.sensors[0].measure_and_update()
+        dic = sa.sensors[0].distance_in_cm
+        lic = sa.sensors[0].length_in_cm
+        print("Distance in cm: " + str(dic) + ".\r\n")
+        print("Length in cm: " + str(lic) + ".\r\n")
+    elif cmd == "calibrate": # axis diff doesn't get changed here with new dtfic value -> only in calibrate_all, no writing to file here -> no specific change possible 
+        print("Calibrating sensor...")
+        sa.sensors[0].calibrate()
+        dtfic = sa.sensors[0].distance_to_floor_in_cm
+        print("Distance to floor in cm: " + str(dtfic) + ".\r\n")
     elif cmd == "measure all":
         print("Measuring all sensors...")
+        sa.measure_all()
+        ml = sa.max_length
+        mls = sa.max_length_sensor
+        if (mls is not None):
+            mlsid = mls.id
+        print("Max length in cm: " + str(ml) + ".\r\n")
+        print("Max length sensor ID: " + str(mlsid) + ".\r\n")
+    elif cmd == "calibrate all":
+        print("Calibrating all sensors...")
+        sa.calibrate_all()
+        fbd = sa.front_back_diff
+        lrd = sa.left_right_diff
+        yac = sa.y_axis_calibrated
+        xac = sa.x_axis_calibrated
+        if (yac):
+            print("Front-back diff in cm: " + str(fbd) + ".\r\n")
+        else:
+            print("Front-back diff in cm: Not calibrated.\r\n")
+        if (xac):
+            print("Left-right diff in cm: " + str(lrd) + ".\r\n")
+        else:
+            print("Left-right diff in cm: Not calibrated.\r\n")
+        with open("calibration.txt", "w") as f:
+            f.write("")
         for sensor in sa.sensors:
-            sensor.measure()
-            print(f"Sensor {sensor.id}: {sensor.distance_in_cm} cm.")
+            print(f"{sensor.id}: {sensor.distance_to_floor_in_cm} cm.")
+            with open("calibration.txt", "a") as f:
+                f.write(f"{sensor.id}: {sensor.distance_to_floor_in_cm}\n")
+    elif "multishot" in cmd:
+        shots = 3 # Default value
+        sc = cmd.split()
+        for part in sc:
+            if part != "multishot":
+                try:
+                    shots = int(part)
+                except ValueError:
+                    pass
+        print(f"Shots: {shots}.")
+        print("Measuring all sensors multiple times...")
+        sa.update_all(shots)
+        aml = sa.average_max_length
+        print("Average max length in cm: " + str(aml) + ".\r\n")
+    elif "stabilize" in cmd:
+        shots = 3 # Default value
+        sc = cmd.split()
+        for part in sc:
+            if part != "stabilize":
+                try:
+                    shots = int(part)
+                except ValueError:
+                    pass
+        print(f"Shots: {shots}.")
+        print("Calibrating all sensors multiple times...")
+        sa.stabilize(shots)
+        afbd = sa.average_front_back_diff
+        alrd = sa.average_left_right_diff
+        yac = sa.y_axis_calibrated
+        xac = sa.x_axis_calibrated
+        if (yac):
+            print("Average front-back diff in cm: " + str(afbd) + ".\r\n")
+        else:
+            print("Average front-back diff in cm: Not calibrated.\r\n")
+        if (xac):
+            print("Average left-right diff in cm: " + str(alrd) + ".\r\n")
+        else:
+            print("Average left-right diff in cm: Not calibrated.\r\n")
+        with open("calibration.txt", "w") as f:
+            f.write("")
+        for sensor in sa.sensors:
+            print(f"{sensor.id}: {sensor.distance_to_floor_in_cm} cm.")
+            with open("calibration.txt", "a") as f:
+                f.write(f"{sensor.id}: {sensor.distance_to_floor_in_cm}\n")
